85a6f211ac130754726c9091052f14f569832964
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-02 09:53:47 +0300

    Initial Commit

---
+++ Movietrending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movietrending
+//
+//  Created by Ильфат Салахов on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
